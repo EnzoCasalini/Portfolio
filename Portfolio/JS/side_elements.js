99b7a16da81b31af5d824ff44d5231ff77d409fa
Enzo <enzocasa83@gmail.com>
2022-02-23 16:59:11 +0100

    Regroupement en dossiers

--- Portfolio/JS/side_elements.js
+++ Portfolio/JS/side_elements.js
@@ -1,9 +1,13 @@
 window.onscroll = function() {ScrollApparition()};
 function ScrollApparition() {
   $Apropos = document.getElementById("A_propos");
+  $right = document.getElementById("scrollElementRight");
+  $left = document.getElementById("scrollElementLeft");
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) 
   {
     $Apropos.className = "apparition";
+    $right.className = "right_contacts";
+    $left.className = "left_contacts";  
   } 
   else {
     $Apropos.className ="invisible";
