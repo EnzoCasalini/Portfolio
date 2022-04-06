cabf11a82015d1d13c0a10b5b5c97bf5c91a3369
Enzo Casalini <enzocasa83@gmail.com>
2022-04-06 22:12:31 +0200

    Ajout projet

--- Portfolio/JS/navbar_apparition.js
+++ Portfolio/JS/navbar_apparition.js
@@ -1,26 +1,28 @@
 const body = document.body;
 let lastScroll = 0;
 
-window.addEventListener('scroll', () => {
-    const currentScroll = window.scrollY;
-
-    if (currentScroll <= 0)
-    {
-        body.classList.remove("scroll-up");
-    }
-
-    if (currentScroll > lastScroll && !body.classList.contains("scroll-down"))
-    {
-        body.classList.remove("scroll-up");
-        body.classList.add("scroll-down");
-    }
-
-    if (currentScroll < lastScroll && body.classList.contains("scroll-down"))
-    {
-        body.classList.remove("scroll-down");
-        body.classList.add("scroll-up");
-    }
-
-
-    lastScroll = currentScroll;
-})
+if (window.matchMedia("(min-width: 992px)").matches) {
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.scrollY;
+    
+        if (currentScroll <= 0)
+        {
+            body.classList.remove("scroll-up");
+        }
+    
+        if (currentScroll > lastScroll && !body.classList.contains("scroll-down"))
+        {
+            body.classList.remove("scroll-up");
+            body.classList.add("scroll-down");
+        }
+    
+        if (currentScroll < lastScroll && body.classList.contains("scroll-down"))
+        {
+            body.classList.remove("scroll-down");
+            body.classList.add("scroll-up");
+        }
+    
+    
+        lastScroll = currentScroll;
+    })
+  } 
